ed5dc6fd6e411ddfaaf267c8fef8a30d8588794c
Adam Elhamami <adamel99@gmail.com>
2023-05-12 13:19:21 -0400

    login and logout

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
@@ -16,8 +18,6 @@ router.use('/users', usersRouter);
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
     where: {
