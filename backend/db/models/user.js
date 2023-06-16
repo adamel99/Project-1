931ebf61a95d4c94e1a7f962536df9b3f68a6f24
Adam Elhamami <adamel99@gmail.com>
2023-06-15 21:32:41 -0400

    fixed errors

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,7 +5,7 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      User.hasMany(models.Group, { foreignKey: 'organizerId'})
+      User.hasMany(models.Group, { foreignKey: 'userId'})
     }
   };
   User.init(
